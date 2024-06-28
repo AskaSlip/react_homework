1b7b5708f10e8a3ef833126de38448759fbd48c4
AskaSlip <varenaska@gmail.com>
2024-06-28 19:14:56 +0400

    add Promise type

--- src/services/api.service.tsx
+++ src/services/api.service.tsx
@@ -1,14 +1,16 @@
 import axios from "axios";
+import {IUser} from "../models/IUser";
+import {IPost} from "../models/IPost";
 
 let axiosInstance = axios.create({
     baseURL: 'https://dummyjson.com'
 })
 
-const getAllUsers = async ()=> {
+const getAllUsers = async () : Promise<IUser[]>=> {
     return await axiosInstance.get('/users'+'?limit=0').then((response) => response.data.users)
 };
 
-const getAllPostsByUserId = async (id:number)=> {
+const getAllPostsByUserId = async (id:number) : Promise<IPost[]>=> {
     return await axiosInstance.get('/posts/user/'+id).then((response) => response.data.posts)
 }
 
