5b78b71f52f0b71d0b27d2cdce6abb78c6eafc60
AskaSlip <varenaska@gmail.com>
2024-07-04 21:57:15 +0400

    homework 4 with class component on branch hw_less 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import UsersComponent from "./components/users-component/UsersComponent";
 
 function App() {
   return (
-    <>
-
-    </>
+    <div>
+      <UsersComponent/>
+    </div>
   );
 }
 
