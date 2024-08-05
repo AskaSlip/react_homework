3ed13ad5589cd52d70e7958deb85b2c6c4f1ac60
AskaSlip <varenaska@gmail.com>
2024-08-06 01:33:44 +0400

    hw12 base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import {useSelector} from "react-redux";
+import {decrement, increment, incrementByAmount, useAppDispatch, useAppSelector} from "./redux/store";
 
 function App() {
-  return (
-    <>
 
-    </>
+let {value} = useAppSelector(state => state.counter1SliceState);
+
+let dispatch = useAppDispatch();
+
+
+  return (
+    <div>
+      <h2>{value}</h2>
+        <button onClick={() => {
+            dispatch(increment())
+        }}>increment by 1</button>
+        <button onClick={() => {
+            dispatch(decrement())
+        }}>
+            decrement by 1
+        </button>
+        <button onClick={() => {
+            dispatch(incrementByAmount(100))
+        }}>increment by 100</button>
+
+    </div>
   );
 }
 
